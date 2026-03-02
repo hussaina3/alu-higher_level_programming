b9dd63418bb6bc985f177dba2c372cfffa054e41
hussaina3 <a.hussaina@alustudent.com>
2026-03-02 20:19:13 +0200

    completed import_modules tasks

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+cat > 3-infinite_add.py << 'EOF'
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    total = 0
+    for i in range(1, len(argv)):
+        total += int(argv[i])
+    print(total)
+EOF
---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,5 @@
+cat > 5-variable_load.py << 'EOF'
+#!/usr/bin/python3
+from variable_load_5 import a
+print(a)
+EOF
