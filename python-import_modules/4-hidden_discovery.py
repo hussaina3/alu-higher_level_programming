c6afb3edd010101571b7c451d13d7715f413742c
hussaina3 <a.hussaina@alustudent.com>
2026-03-02 20:44:53 +0200

    fix task 4 hidden discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
-cat > 4-hidden_discovery.py << 'EOF'
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
     for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
-EOF
